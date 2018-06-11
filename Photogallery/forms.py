59f689da01c82c581df70fb39fdbd66621153d83
elison vrioni <elisonvrioni@hotmail.it>
2018-06-11 17:25:11 +0200

    pictures uploaded

--- Photogallery/forms.py
+++ Photogallery/forms.py
@@ -1,19 +1,12 @@
 from django import forms
-
 from Photogallery.models import Item
-
-
 from django import forms
 from django.forms import ModelForm
 
-
-
-
-
 class UploadFileForm(ModelForm):
-    class Meta:
-        model =Item
-        fields = ['name', 'description', 'image', 'category']
+  class Meta:
+    model =Item
+    fields = ['name', 'description', 'image', 'category']
 
 form = UploadFileForm()
 
